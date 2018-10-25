6ac6a77a6faa8866b83eb0253642a92fb39f8905
Sohail Hameed <3370sohail@gmail.com>
2018-10-24 22:38:50 -0400

    Set up 2 more DB models and their basic APIs (#28)
    * Created models for projects and user_associations tables
    
    * Fixed minor Model errors and created create and list routes for projects and user_associations models
    
    * changed project route error message

--- node/server/controllers/users.js
+++ node/server/controllers/users.js
@@ -1,6 +1,7 @@
 const Users = require('../models').users;
 
 module.exports = {
+  //create a new user
   create(req, res) {
     return Users
       .create({
@@ -15,4 +16,44 @@ module.exports = {
       .then(users => res.status(201).send(users))
       .catch(error => res.status(400).send(error));
   },
+
+  // list all users
+  list(req, res) {
+    return Users
+      .findAll({
+        attributes: {exclude: ['password', 'createdAt', 'updatedAt'] },
+        order: [
+          ['createdAt', 'DESC'],
+        ],
+      })
+      .then((users) => res.status(200).send(users))
+      .catch((error) => res.status(400).send(error));
+  },
+
+  update(req, res) {
+  return Users
+    .findById(req.body.id, {
+      attributes: {exclude: ['password', 'createdAt', 'updatedAt'] },
+    })
+    .then(users => {
+      if (!users) {
+        return res.status(404).send({
+          message: 'User Not Found',
+        });
+      }
+      return users
+        .update({
+          name: req.body.name,
+          bio: req.body.bio,
+          email: req.body.email,
+          photo: req.body.photo,
+          linked_in: req.body.linked_in,
+          github: req.body.github
+        })
+        .then((users) => res.status(200).send(users))
+        .catch((error) => res.status(400).send(error));
+    })
+    .catch((error) => res.status(400).send(error));
+},
+
 };
