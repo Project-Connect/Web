5f291ba8b4df664c26b382eee303f013f1bff5af
Alex Shin <jaewon.shin@mail.utoronto.ca>
2018-10-30 12:48:30 -0400

    Implemented mini project view boilerplate code (#43)
    Implemented mini project view boilerplate code

--- react/src/components/examples/reduxExamples/examplePage.js
+++ react/src/components/examples/reduxExamples/examplePage.js
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
 //NOTE: Always use PacalCase for react components
 import ExampleComponent from "./exampleComponent"
 import ExampleComponentTwo from "./exampleComponentTwo"
+import MiniProjectComponent from "../../miniProjectView/miniProjectComponent"
 
 class ReduxExamples extends Component {
   render() {
@@ -14,6 +15,8 @@ class ReduxExamples extends Component {
         then click on the "Click Me" button to see redux in action
         <ExampleComponent/>
         <ExampleComponentTwo/>
+        <MiniProjectComponent id="1"/>   
+
       </div>
     );
   }
---
+++ react/src/components/miniProjectView/miniProjectComponent.js
@@ -0,0 +1,42 @@
+/*
+Component to summarize a project in its miniaturized format.
+It contains info of the project's title, description, and stacks.
+*/
+import React, { Component } from "react";
+
+import './miniProjectComponent.css';
+
+class MiniProjectComponent extends Component {
+
+    render() {
+
+        const modalStyle = {
+        backgroundColor: '#cdcdcd',
+        borderRadius: 5,
+        maxWidth: 700,
+        minHeight: 150,
+        margin: '0 auto',
+        padding: 30
+        };
+
+        return (
+
+        <div className="modal" style={modalStyle}>
+            <div className="txtTitle" >
+                Title
+            </div>
+            <br />
+            <div>
+                Project Description
+            </div>
+            <br />
+            <div>
+                Stacks
+            </div>
+        </div>
+
+        );
+  }
+}
+
+export default MiniProjectComponent
