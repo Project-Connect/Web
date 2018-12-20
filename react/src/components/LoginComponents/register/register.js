4bcb5c42f47dcd5c9ae1686c1f52350bb3fe6578
3370sohail <3370sohail@gmail.com>
2018-12-20 16:21:37 -0500

    temp student register

--- react/src/components/LoginComponents/register/register.js
+++ react/src/components/LoginComponents/register/register.js
@@ -12,8 +12,7 @@ class Register extends React.Component{
         this.state = {
             username: "",
             passworsd: "",
-            email: "",
-            type: "student"
+            email: ""
         }
         this.navigate = this.navigate.bind(this);
     }
@@ -23,7 +22,7 @@ class Register extends React.Component{
             this.props.showError("Please fill in input field(s)")
         }
         else {
-            let url = process.env.API_URL + "/api/users/" + this.state.type;
+            let url = process.env.API_URL + "/api/users/student" + this.state.type;
             fetch(url, {
                 method: "POST",
                 headers: {
