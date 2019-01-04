48d5c4ce39cdb18a29996b3bafb49ffebc1242ed
Daniel Chen <chen.daniel2099@gmail.com>
2019-01-04 16:57:35 -0500

    name mismatch fix

--- node/server/routes/index.js
+++ node/server/routes/index.js
@@ -27,7 +27,6 @@ const adminAuthenticate = (req, res, next) => {
 
 module.exports = (app) => {
   app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
     res.header('Access-Control-Allow-Credentials',' true');
     next();
