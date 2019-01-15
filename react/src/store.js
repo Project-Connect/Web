d7f661961f3933a82345c5c4cb9677af7e6a49ae
Daniel Chen <chen.daniel2099@gmail.com>
2019-01-15 17:26:20 -0500

    removing logger from production

--- react/src/store.js
+++ react/src/store.js
@@ -3,12 +3,7 @@ This file is responsible for creating the redux store and injecting middleware
 */
 import { applyMiddleware, createStore } from 'redux';
 import reducer from "./reducers"
-
-// this is a used for easy debugging via browser console, with colorful displays
-// to highlight state before and after transition.
-import logger from "redux-logger"
-
 import apiHandler from "./custom_redux_middleware/api_call_handler"
 
-const middleware = applyMiddleware(apiHandler, logger)
+const middleware = applyMiddleware(apiHandler)
 export default createStore(reducer, middleware)
